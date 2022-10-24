95097fdc4a0d146a832bdcada100509e355dc93a
iamaredfoxx <iamaredfoxx@gmail.com>
2022-10-24 21:51:16 +0700

    Rename hostname to lower case

--- hostList.go
+++ hostList.go
@@ -2,6 +2,7 @@ package route
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 )
@@ -15,9 +16,10 @@ type hostList struct {
 }
 
 func (hl *hostList) register(hostname string, config *HostConfiguration) error {
+	hostname = strings.ToLower(hostname)
 	h, _ := hl.host(hostname)
 	if h != nil {
-		return errors.New("host already defined")
+		return errors.New("host already defined: " + hostname)
 	}
 	h = &host{
 		hostname: hostname,
@@ -33,7 +35,7 @@ func (hl *hostList) register(hostname string, config *HostConfiguration) error {
 }
 
 func (hl *hostList) host(hostname string) (*host, error) {
-	h, isOk := hl.hosts[hostname]
+	h, isOk := hl.hosts[strings.ToLower(hostname)]
 	if !isOk {
 		return nil, errors.New("host not found")
 	}
